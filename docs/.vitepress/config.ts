96e6e9aaaa73a8c857ccd6c7219888c3ec77e504
gezhaoyou <gezhaoyou@126.com>
2023-07-20 23:45:48 +0800

    Init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "PixPark",
+  description: "Real-time audio and video processing toolset for developers",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Docs', link: '/docs' },
+      { text: 'Blog', link: '/blog' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Docs',
+        items: [
+          { text: 'GPUPixel', link: '/docs' },
+          { text: 'PanoCam', link: '/panocam-docs' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'twitter', link: 'https://twitter.com/PixParkOSS' },
+      { icon: 'youtube', link: 'https://www.youtube.com/@pixpark' },
+      { icon: 'github', link: 'https://github.com/pixpark' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "PixPark"
+  text: "Open source toolset for audio and video"
+  tagline: Welcome to contribute code and suggestions
+  actions:
+    - theme: brand
+      text: Docs
+      link: /docs
+    - theme: alt
+      text: Github
+      link: https://github.com/pixpark
+
+features:
+  - title: GPUPixel
+    details: Video and image processing library like GPUImage support iOS | macOS | Android
+  - title: PanoCam
+    details: Pano Camera like google camera for Android.
+  - title: AlitaPlayer
+    details: Yet another video player, support iOS | Android | macOS | Win
+---
+
